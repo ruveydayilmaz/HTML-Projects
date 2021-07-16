bb7c40a0c04e6c9e82308c9e6a55e790a6be5900
ruveydayilmaz <nur.yilmaz@bil.omu.edu.tr>
2021-07-16 15:54:20 +0300

    modify example5
    add sub-pages
    add form-handler

---
+++ example5/form-handler.php
@@ -0,0 +1,29 @@
+<?php
+
+ /*if I'm not wrong, you need to have a domain name to get the email.
+ So I'm skipping that process and just adding this piece of code to look at later */
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n". 
+                "User Message: $message .\n";
+
+$to = 'yourgmail';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
